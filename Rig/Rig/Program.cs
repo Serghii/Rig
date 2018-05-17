c5a0b125fedd8759e4b1290a71a9d171eee43d36
Serg <vpfonya@gmail.com>
2018-05-17 13:03:16 +0300

    test

---
+++ Rig/Rig/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
